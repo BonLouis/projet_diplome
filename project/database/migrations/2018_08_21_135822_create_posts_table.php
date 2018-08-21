10d4b36d98ab27216099a849ae2fe84d68840ef8
Louis.Bon(l) <bon.louis765@gmail.com>
2018-08-21 17:08:24 +0200

    Make all migrations

---
+++ project/database/migrations/2018_08_21_135822_create_posts_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->enum('post_type', ['stage', 'formation']);
+            $table->text('description');
+            $table->decimal('price', 7, 2)->nullable();
+            $table->smallInteger('max_seats')->nullable();
+            $table->dateTime('begin_at')->nullable();
+            $table->dateTime('end_at')->nullable();
+            $table->enum('status', ['draft', 'published', 'trash']);
+            $table->boolean('open');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
---
+++ project/database/migrations/2018_08_21_140053_create_post_registration_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostRegistrationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post_registration', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->unsignedInteger('post_id');
+            $table->foreign('post_id')
+                ->references('id')
+                ->on('posts');
+
+            $table->unsignedInteger('registration_id');
+            $table->foreign('registration_id')
+                ->references('id')
+                ->on('registrations');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post_registration');
+    }
+}
