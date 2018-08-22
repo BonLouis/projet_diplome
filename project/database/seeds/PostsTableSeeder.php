d2abab1bc99ce8e49c6f46ac16551bf67df29176
tour <bon.louis765@gmail.com>
2018-08-23 00:15:12 +0200

    Make seeds and factories + .gitignore changes
    All seeds and factories are done, we can go on.

---
+++ project/database/seeds/PostsTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(App\Post::class, 10)->create()->each(function (App\Post $post) {
+            $categories = App\Category::pluck('id')->shuffle();
+
+            $post->categories()->attach($categories->slice(0, rand(1, $categories->count())));
+            $post->picture()->save(factory(App\Picture::class)->make());
+        });
+    }
+}
