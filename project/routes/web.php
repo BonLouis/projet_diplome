3be5a5c2b874cf19d5fccfd9bae6ca020da7b39e
Louis.Bon(l) <bon.louis765@gmail.com>
2018-08-23 14:44:33 +0200

    Router
    Add routes including secure ones under /admin.
    This will probably evolved.

--- project/routes/web.php
+++ project/routes/web.php
@@ -11,6 +11,18 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+Auth::routes();
+
+Route::get('/', 'FrontController@index');
+
+Route::get('/post/{id}', 'FrontController@show')->where(['id'=>'[0-9]+']);
+
+Route::get('/category/{id}', 'FrontController@showPostByCategory')->where(['id'=>'[0-9]+']);
+
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
+	Route::resource('category', 'CategoryController')->only(['index', 'create', 'edit']);
+	Route::resource('post', 'PostController')->only(['index', 'create', 'edit']);
 });
+
+Route::get('/home', 'HomeController@index')->name('home');
