97bbbafa0fcf49eb1145cf599273f1fb1518ab7e
Louis.Bon(l) <bon.louis765@gmail.com>
2018-08-25 00:49:47 +0200

    Changing routes for the future

--- project/routes/web.php
+++ project/routes/web.php
@@ -16,12 +16,13 @@ Auth::routes();
 
 Route::get('/', 'FrontController@index');
 
-Route::get('/post/{id}', 'FrontController@show')->where(['id'=>'[0-9]+']);
+Route::get('/post/{post}', 'FrontController@show')->where(['post'=>'[0-9]+'])->name('showPost');
 
-Route::get('/category/{id}', 'FrontController@showPostByCategory')->where(['id'=>'[0-9]+']);
+// TODO
+// Route::get('/category/{category}', 'FrontController@showPostByCategory')->where(['category'=>'[0-9]+']);
 
 Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
-	Route::resource('category', 'CategoryController')->only(['index', 'create', 'edit']);
+	// Route::resource('category', 'CategoryController')->only(['index', 'create', 'edit']);
 	Route::resource('post', 'PostController')->only(['index', 'create', 'edit']);
 });
 
