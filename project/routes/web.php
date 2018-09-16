f0fe3be9ec3fdf01f17683bc32a71d0f80758b07
Louis <louis.bon@autovisual.com>
2018-09-16 21:48:30 +0200

    Somes new routes to handle ajax request

--- project/routes/web.php
+++ project/routes/web.php
@@ -14,16 +14,26 @@
 
 Auth::routes();
 
-Route::get('/', 'FrontController@index');
+Route::get('/', 'FrontController@index')->name('home');
 
 Route::get('/post/{post}', 'FrontController@show')->where(['post'=>'[0-9]+'])->name('showPost');
 
+Route::get('/stages', 'FrontController@showStages')->name('showStages');
+Route::get('/formations', 'FrontController@showFormations')->name('showFormations');
+Route::get('/contact', 'FrontController@showContact')->name('contact');
 // TODO
 // Route::get('/category/{category}', 'FrontController@showPostByCategory')->where(['category'=>'[0-9]+']);
 
-Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
-	// Route::resource('category', 'CategoryController')->only(['index', 'create', 'edit']);
-	Route::resource('post', 'PostController')->only(['index', 'create', 'edit']);
-});
+// Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
+// 	// Route::resource('category', 'CategoryController')->only(['index', 'create', 'edit']);
+// 	Route::resource('post', 'PostController')->only(['index', 'create', 'edit']);
+// });
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::middleware('auth')->namespace('Admin')->prefix('admin')->group(function() {
+	Route::resource('post', 'PostController');
+
+	// This route will be used with Ajax
+	Route::get('trash/{post}', 'PostController@trash')->where(['post'=>'[0-9]+'])->name('trash');
+	Route::get('loadOneAndEdit/{post}', 'PostController@loadOneAndEdit')->where(['post'=>'[0-9]+'])->name('loadOneAndEdit');
+});
+// Route::get('/home', 'HomeController@index')->name('home');
